cc27512119008075d4b229bfd55081c7a21fd185
Christian Fobel <christian@fobel.net>
2017-11-30 13:14:13 -0500

    feat(install): copy full contents, link to exe

--- {{cookiecutter.project_slug}}/bld.bat
+++ {{cookiecutter.project_slug}}/bld.bat
@@ -12,6 +12,15 @@ tar xvf %FILENAME% -C "%PKG_NAME%"
 {% endif %}
 {% endif %}
 if errorlevel 1 exit 1
-REM Copy contents to ${PREFIX}\Library\bin
-REM TODO xcopy /S /Y /I /Q %PKG_NAME% "%PREFIX%"\...
+
+REM Copy full archive contents to Conda environment.
+set INSTALL_PATH=%PREFIX%/library/opt/%PKG_NAME%
+xcopy /S /Y /I /Q %PKG_NAME% %INSTALL_PATH%
+
+REM Make link to main executable in `Scripts` directory.
+set EXE_PATH=%CONDA_PREFIX%/library/opt/%PKG_NAME%/%PKG_NAME%.exe
+set BAT_PATH=%PREFIX%/Scripts/%PKG_NAME%.bat
+echo @echo off >> "%BAT_PATH%"
+if errorlevel 1 exit 1
+echo "%EXE_PATH%" %* >> "%BAT_PATH%"
 if errorlevel 1 exit 1
